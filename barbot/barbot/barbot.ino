403f1db3cb09a776a5d33f1bc2a89eb6bd2fa74b
Mike Miles <mikemiles86@gmail.com>
2013-10-02 21:25:31 -0400

    - update barbot code to respond to relay states.

--- barbot/barbot/barbot.ino
+++ barbot/barbot/barbot.ino
@@ -1,27 +1,31 @@
-#include <Servo.h>
-
-const byte connectedServos = 5;
+const byte connectedRelays = 4;
 int starting_pin = 2;
-//Servo myServos[connectedServos];
-int myServos[connectedServos];
+int delayTime = 500;
+int myRelays[connectedRelays];
 
 void setup(){
-  for(int s=0; s<connectedServos;s++){
-    //myServos[s].attach(starting_pin+2); //set to pin s+2 (starts at pin 2)
-    myServos[s] = starting_pin + s;
-    pinMode(myServos[s],OUTPUT);
-    toggleServo(s,0); //close pin on setup
+  // initialize each relay 
+  for (int s=0; s < connectedRelays; s++){
+    //set relay pin to s+starting_pin 
+     myRelays[s] = starting_pin + s;
+     pinMode(myRelays[s], OUTPUT);
+     //start the pin high, so relay is open
+     toggleRelay(s,1); 
   }
-  Serial.begin(9600); //begin serial output
+  
+  Serial.begin(9600); //begin serial output to send/recieve commands
 }
 
 void loop(){
-  //read from Serial input
+  //read from Serial input for command
   if (Serial.available()){
+    //run the command if found and valid
     runCommand(readCommand());
   }
-  delay(500); //wait .5sec before looping again.
+  //delay for delay timesec.
+  delay(delayTime);
 }
+
 
 String readCommand(){
   String commandLine = "";
@@ -40,15 +44,15 @@ void runCommand(String commandLine){
   int index = commandLine.indexOf(" ");
   String command = commandLine.substring(0,index);
   String parameters = commandLine.substring(index+1);
-  
+  Serial.println(commandLine);
   if (command == "commands"){
     listCommands();
-  }else if (command == "valves"){
-    listValves();
+  }else if (command == "relays"){
+    listRelays();
   }else if (command == "open"){
-     toggleValve(parameters,1);
+     toggleRelay(parameters,1);
   }else if (command == "close"){
-    toggleValve(parameters,0);
+    toggleRelay(parameters,0);
   }else{
     printError("Unknown command\""+command+"\"");
   }
@@ -59,67 +63,68 @@ void listCommands(){
   String msg;
   msg = "\n";
   //msg =msg + "commands: //list of commands\n";
-  msg =msg + "valves: //displays the number of valves and thier pins\n";
-  msg =msg + "open: [n [s]] //open valve n (optional) close after  s seconds\n";
-  msg =msg + "close: [n [s]] //close valve n (optional) after s seconds\n";
+  msg =msg + "relays: //displays the number of relays and thier pins\n";
+  msg =msg + "open: [n [s]] //open relay n (optional) close after  s seconds\n";
+  msg =msg + "close: [n [s]] //close relay n (optional) after s seconds\n";
   
   msg =msg + "pass\"help\" as parameter for info on command";
 
   printSuccess(msg);
 }
 
-void listValves(){
+void listRelays(){
   String msg = "\n";
-   for(int s=0; s<connectedServos;s++){
+   for(int s=0; s<connectedRelays;s++){
      msg =msg + s +": pin "+(starting_pin + s) + "\n";
    }
   printSuccess(msg);
 }
 
 
-void toggleValve(String parameters,int open_valve){
+void toggleRelay(String parameters,int relay_voltage){
   String msg = "";
   if (parameters == "help"){
-    msg += (open_valve ? "open":"close");
+    msg += (relay_voltage ? "open":"close");
     msg +=" [valve_num] [seconds]";
     printSuccess(msg);
   }else{
-    int servo = -1;
+    int relay = -1;
     int seconds = -1;
     int index = parameters.indexOf(" ");
     if(index > 0){
-      servo  = stringToInt(parameters.substring(0,index));
+      relay  = stringToInt(parameters.substring(0,index));
       seconds = stringToInt(parameters.substring(index+1));
     }else{
-      servo = stringToInt(parameters);
+      relay = stringToInt(parameters);
     }
    
-    if ((servo >= 0) && (servo < connectedServos)){
-      msg = "Valve " + (String)servo;
-      if (open_valve){
-        msg =msg + " opened and";
-        toggleServo(servo,1);
+    if ((relay >= 0) && (relay < connectedRelays)){
+      msg = "Relay " + (String)relay;
+      if (relay_voltage){
+        msg =msg + " opened";
+        toggleRelay(relay,1);
       }
  
       if (seconds > 0){
         delay(seconds*1000);
-        msg =msg + " after " + (String)seconds;
+        msg =msg +(relay_voltage ? " after ":"") + (String)seconds;
       }
       
-      if (!open_valve || (open_valve && seconds>0)){
+      if (!relay_voltage || (relay_voltage && seconds>0)){
         msg= msg + " closed";
-        toggleServo(servo,0);
+        toggleRelay(relay,0);
       }
       if (seconds > 0){
         msg =msg + " after " + (String)seconds;
       }      
       printSuccess(msg);
   }else{
-      msg = "Invalid valve number ("+ (String)servo + ")";
+      msg = "Invalid valve number ("+ (String)relay + ")";
       printError(msg);
     }
   }
 }
+
 
 void printSuccess(String message){
   message = "Success: " + message;
@@ -131,12 +136,11 @@ void printError(String message){
   Serial.println(message);
 }
 
-void toggleServo(int servo,int open_servo){
- // myServos[servo].write((open_servo ? HIGH:LOW));
- if (open_servo){
-   digitalWrite(myServos[servo],HIGH);
+void toggleRelay(int relay,int relay_voltage){
+ if (relay_voltage){
+   digitalWrite(myRelays[relay],HIGH);
  }else{
-   digitalWrite(myServos[servo],LOW);
+   digitalWrite(myRelays[relay],LOW);
  }
 }
 
@@ -148,3 +152,5 @@ int stringToInt(String mystring){
   return n;
 }
 
+
+  
